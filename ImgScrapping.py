3e50a56b85bd84dfa84a84ebdec3576a21ac8a21
sid <sid@adoltech.com>
2023-08-24 14:52:32 +0530

    Minor Changes

--- ImgScrapping.py
+++ ImgScrapping.py
@@ -8,21 +8,21 @@ from selenium import webdriver
 from colorama import Fore,Style
 from selenium.webdriver.common.by import By
 
-t1 = time.time()
-a = 0
-b = -1
+imgLabel = 0
+queryPos = -1
 imgSet = set()
 imgTempVal = 0
+t1 = time.time()
 driver = webdriver.Firefox()
-q = ['<Search Keyword Query>', '<Search Keyword Query>', '<Search Keyword Query>', '<Search Keyword Query>']
+q = ['<Search Keyword Query>', '<Search Keyword Query>', '<Search Keyword Query>', '<Get some More Queries On!>']
 f = open("LinkSet.txt", "a")
 f.write("\n\n\n\n\n\n\n\n\n\n")
 f.write(str(datetime.datetime.now())+'\n')
 f.close()
 for q_ in q:
-    b+=1
     skip = 0
     max_ = 150
+    queryPos+=1
     imgTempVal = 1
     imgTemp = set()
     driver.get("https://www.google.com/search?q={}&tbm=isch&source=hp&biw=1873&bih=969&ei=jX1tY83SF87az7sPwKO68AY&iflsig=AJiK0e8AAAAAY22LnaUQStNMEm2fk8dCGptfVOdHqY1D&ved=0ahUKEwiNzZqI16T7AhVO7XMBHcCRDm4Q4dUDCAc&uact=5&oq=cat&gs_lcp=CgNpbWcQAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzIICAAQgAQQsQMyCAgAEIAEELEDMggIABCABBCxAzILCAAQgAQQsQMQgwEyCAgAEIAEELEDMggIABCABBCxAzIFCAAQgAQ6CAgAELEDEIMBUABYrQFgvgdoAHAAeACAAU-IAewBkgEBM5gBAKABAaoBC2d3cy13aXotaW1n&sclient=img".format(q_))
@@ -31,14 +31,14 @@ for q_ in q:
         time.sleep(2)
         if skip>100:
             break
-        p = 0
+        picSelect = 0
         try:
             comPics = driver.find_elements(By.CLASS_NAME,"Q4LuWd")
             for img in comPics[len(imgTemp)+skip:max_]:
-                p+=1
-                if p%10==0:
+                picSelect+=1
+                if picSelect%10==0:
                     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                    time.sleep(5)
+                    time.sleep(3)
                 if skip>100:
                     break
                 img.click()
@@ -58,8 +58,8 @@ for q_ in q:
                         max_+=1
                         break
                     if 'http' in pic.get_attribute('src') and 'encrypted' not in pic.get_attribute('src') and pic.get_attribute('src') not in imgTemp:
-                        x = len(imgSet)+b*imgTempVal
-                        y = len(imgTemp)+b*imgTempVal
+                        x = len(imgSet)+queryPos*imgTempVal
+                        y = len(imgTemp)+queryPos*imgTempVal
                         imgTemp.add(pic.get_attribute('src'))
                         print(Fore.BLUE+Style.BRIGHT+"\tScrapped Img{}~Img{} Url...".format(str(x+y),str(y)))
                         print(Fore.GREEN+Style.BRIGHT+pic.get_attribute('src'))
@@ -81,18 +81,18 @@ print("\n\n\nFetchingExecTime:", t2-t1)
 t1 = time.time()
 def saving(img_):
     try:
-        global a
+        global imgLabel
         t1_ = time.time()
         r = requests.get(img_).content
         f = io.BytesIO(r)
         imgf = Image.open(f)
-        imgf.save("ImgSet/"+str(a)+'.png')
+        imgf.save("ImgSet/"+str(imgLabel)+'.png')
         t2_ = time.time()
-        if a%2==0:
-            print(Fore.MAGENTA+Style.BRIGHT+str(a)+'.png in! ~ '+Fore.BLUE+Style.BRIGHT+str(t2_-t1_)+' Secs')
+        if imgLabel%2==0:
+            print(Fore.MAGENTA+Style.BRIGHT+str(imgLabel)+'.png in! ~ '+Fore.BLUE+Style.BRIGHT+str(t2_-t1_)+' Secs')
         else:
-            print(Fore.CYAN+Style.BRIGHT+'\t\t'+str(a)+'.png in! ~ '+Fore.BLUE+Style.BRIGHT+str(t2_-t1_)+' Secs')
-        a+=1
+            print(Fore.CYAN+Style.BRIGHT+'\t\t'+str(imgLabel)+'.png in! ~ '+Fore.BLUE+Style.BRIGHT+str(t2_-t1_)+' Secs')
+        imgLabel+=1
     except:
         print(Fore.RED+Style.BRIGHT+"~~~|",i)
         pass
@@ -103,4 +103,4 @@ for i in imgSet:
     ThreadObj.join(timeout=60)
               
 t2 = time.time()
-print("\n\n\nExecTime:", t2-t1)
+print("\n\n\nWritingExecTime:", t2-t1)
