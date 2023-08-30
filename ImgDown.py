a0aa119bc9bab0d7d84246b80ffa0dc33ca877fc
sid <sid@adoltech.com>
2023-08-30 17:00:43 +0530

    Some Minor Edits

--- ImgDown.py
+++ ImgDown.py
@@ -1,4 +1,5 @@
 import io
+import os
 import time
 import requests
 import threading
@@ -7,35 +8,36 @@ from colorama import Fore,Style
 
 
 t1=time.time()
-a=0
+imgDown=0
 f=open("DownURLs_set.txt","r")
 s=f.read()
 imgSet=eval(s)
 
-t1=time.time()
-def saving(img_):
+if not os.path.isdir("ImgSet"):
+   os.makedirs("ImgSet")
+
+def saving(imgDown):
     try:
-        global a
-        t1_=time.time()
-        r=requests.get(img_).content
-        f=io.BytesIO(r)
-        imgf=Image.open(f)
-        imgf.save("ImgSet/"+str(a)+'.png')
-        t2_=time.time()
-        if a%2==0:
-            print(Fore.MAGENTA+Style.BRIGHT+str(a)+'.png in! ~ '+Fore.BLUE+Style.BRIGHT+str(t2_-t1_)+' Secs')
+        global imgLabel
+        t1_ = time.time()
+        r = requests.get(imgDown).content
+        f = io.BytesIO(r)
+        imgf = Image.open(f)
+        imgf.save("ImgSet/"+str(imgLabel)+'.png')
+        t2_ = time.time()
+        if imgLabel%2==0:
+            print(Fore.MAGENTA+Style.BRIGHT+str(imgLabel)+'.png in! ~ '+Fore.BLUE+Style.BRIGHT+str(t2_-t1_)+' Secs'+Fore.RESET)
         else:
-            print(Fore.CYAN+Style.BRIGHT+'\t\t'+str(a)+'.png in! ~ '+Fore.BLUE+Style.BRIGHT+str(t2_-t1_)+' Secs')
-        a+=1
+            print(Fore.CYAN+Style.BRIGHT+'\t\t'+str(imgLabel)+'.png in! ~ '+Fore.BLUE+Style.BRIGHT+str(t2_-t1_)+' Secs'+Fore.RESET)
+        imgLabel+=1
     except:
-        print(Fore.RED+Style.BRIGHT+"~~~|",i)
+        print(Fore.RED+Style.BRIGHT+"~~~| "+str(i)+Fore.RESET)
         pass
 
 for i in imgSet:
-    ThreadObj=threading.Thread(target=saving,args=(i,))
+    ThreadObj = threading.Thread(target=saving, args =(i,))
     ThreadObj.start()
-    ThreadObj.join(timeout=60)
-              
-t2=time.time()
-print("\n\n\nExecTime:", t2-t1)
+    ThreadObj.join(timeout=60)       
 
+t2=time.time()
+print("\n\n\nWritingExecTime:", t2-t1)
