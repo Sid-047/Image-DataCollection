d5408d09d1254bef94bbab0d82fc528a205ff5ae
Sid <sid@adoltech.com>
2023-09-03 16:58:31 +0530

    Updated Code Yo

--- URLset_convo.py
+++ URLset_convo.py
@@ -3,6 +3,11 @@ f = open("N:\OpenSourceContribution\Image-DataCollection\LinkSet.txt", "r")
 dataList = ('\n'+f.read()).split("\n\n\n\n\n\n\n\n\n\n\n")[1:]
 
 print("---->", len(dataList))
-for i in dataList:
-    x = i.split('\n')
-    print(x[0])
+dataStampList = [x.split('\n') for x in dataList]
+for i in dataTimeList:
+    print(i[0])
+
+selectStamp = input("Jus' Get the TimeStamp yo:")
+for j in dataStampList:
+    if selectStamp==j[0]:
+        print(j)
