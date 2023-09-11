015cf10b62d69fde77d962f71005e0540175df26
sid <sid@adoltech.com>
2023-09-11 18:53:35 +0530

    Updated Code

--- URLset_convo.py
+++ URLset_convo.py
@@ -1,26 +1,30 @@
-f = open("Image-DataCollection\LinkSet.txt", "r")
-
+f = open("Image-DataCollection\Scraped_LinkSet.txt", "r")
 dataList = ('\n'+f.read()).split("\n\n\n\n\n\n\n\n\n\n\n")[1:]
-
 f.close()
+
 print("---->", len(dataList))
-dataStampList = [x.split('\n') for x in dataList]
-for i in dataStampList:
-    print(i[0])
 
-selectStamp = input("Jus' Get the TimeStamp yo:")
-LinkSet = set()
-for j in dataStampList:
-    if selectStamp in j[0]:
-        for k in j[1:]:
-            if k!='':
-                q_list = eval(k)
-                if isinstance(q_list, list):
-                    LinkSet.add(q_list[1])
+if len(dataList)==0:
+    print("Scrap some Image URLs Yo !")
+    print("There's Nothing in Here")
+else:
+    dataStampList = [x.split('\n') for x in dataList]
+    for i in dataStampList:
+        print(i[0])
 
-print('---------->',len(LinkSet))
-print(LinkSet)
+    selectStamp = input("Jus' Get the TimeStamp yo:")
+    LinkSet = set()
+    for j in dataStampList:
+        if selectStamp in j[0]:
+            for k in j[1:]:
+                if k!='':
+                    q_list = eval(k)
+                    if isinstance(q_list, list):
+                        LinkSet.add(q_list[1])
 
-f = open("Image-DataCollection\DownURLs_set.txt", "w")
-f.write(str(LinkSet))
-f.close()
+    print('---------->',len(LinkSet))
+    print(LinkSet)
+
+    f = open("Image-DataCollection\DownURLs_set.txt", "w")
+    f.write(str(LinkSet))
+    f.close()
