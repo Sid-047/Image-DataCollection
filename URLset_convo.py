100df05a69b03d5fe7fcef51a16e5ff592965d63
Sid <sid@adoltech.com>
2023-09-13 21:06:30 +0530

    Updated Terminal End Stuff

--- URLset_convo.py
+++ URLset_convo.py
@@ -1,3 +1,5 @@
+from colorama import Fore,Style
+
 f = open("Image-DataCollection\Scraped_LinkSet.txt", "r")
 dataList = ('\n'+f.read()).split("\n\n\n\n\n\n\n\n\n\n\n")[1:]
 f.close()
@@ -5,12 +7,12 @@ f.close()
 print("---->", len(dataList))
 
 if len(dataList)==0:
-    print("Scrap some Image URLs Yo !")
+    print(Fore.RED+Style.BRIGHT+"Scrap some Image URLs Yo !"+Fore.RESET)
     print("There's Nothing in Here")
 else:
     dataStampList = [x.split('\n') for x in dataList]
     for i in dataStampList:
-        print(i[0])
+        print(Fore.BLUE+Style.BRIGHT+i[0]+'/n'+Fore.RESET)
 
     selectStamp = input("Jus' Get the TimeStamp yo:")
     LinkSet = set()
@@ -22,9 +24,9 @@ else:
                     if isinstance(q_list, list):
                         LinkSet.add(q_list[1])
 
-    print('---------->',len(LinkSet))
-    print(LinkSet)
+    print(Fore.CYAN+Style.BRIGHT+'URL Count ---------->'+Fore.RESET+str(len(LinkSet)))
 
     f = open("Image-DataCollection\DownURLs_set.txt", "w")
     f.write(str(LinkSet))
     f.close()
+    print("LinkSet's fed to DownURLs_set.txt File Yo!")
